09161660e2443fdcc470a1c9d85338f6f93b49f8
Fouka Régine Gao Tao <foukaregine@gmail.com>
2026-02-24 14:08:58 +0000

    Initial commit edu_crm

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+﻿from flask import Flask
+from app.teachers.route import teachers_bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = "secret"
+
+    app.register_blueprint(teachers_bp)
+
+    return app
---
+++ app/services/teacher_service.py
@@ -0,0 +1,19 @@
+﻿teachers = []
+
+def add_teacher(name, email, speciality):
+    teacher = {
+        'id': len(teachers) + 1,
+        'name': name,
+        'email': email,
+        'speciality': speciality
+    }
+    teachers.append(teacher)
+
+
+def list_teachers():
+    return teachers
+
+
+def delete_teacher(id):
+    global teachers
+    teachers = [t for t in teachers if t['id'] != id]
