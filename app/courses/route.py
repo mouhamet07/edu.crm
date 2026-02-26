fe314a88358fa828ac859fc124e338d431a2cd61
Maryse <maryseokana@gmail.com>
2026-02-26 14:38:09 +0000

    gestion des cas qui peuvent entrainer des erreurs

--- app/courses/route.py
+++ app/courses/route.py
@@ -17,8 +17,11 @@ def test():
 @courses_bp.route("/add")
 @login_required
 def add():
-    add_course("Math", 1)
-    return "Cours ajouté "
+    r1= add_course("Math", 1)
+    if r1 is None:
+        return "Le cours 'Math' existe déjà"
+    else:
+        return f"Le cours '{r1['title']}' a été ajouté avec succès"
 
 @courses_bp.route("/")
 @login_required
@@ -29,11 +32,16 @@ def index():
 @courses_bp.route("/delete/<int:id>")
 @login_required
 def delete(id):
-    delete_course(id)
-    return f"Cours {id} supprimé "
+    result = delete_course(id)
+    return result
 
 @courses_bp.route("/assign/<int:course_id>/<int:student_id>")
 @login_required
 def assign(course_id, student_id):
-    assign_student_to_course(course_id, student_id)
-    return f"Étudiant {student_id} assigné au cours {course_id} "
+    result = assign_student_to_course(course_id, student_id)
+    if result == "not_found":
+        return "Ce cours n'existe pas"
+    elif result == "already":
+        return "L'Étudiant est déjà inscrit"
+    else:
+        return f"L'étudiant {student_id} a été ajouté au cours {course_id}"
--- app/services/course_service.py
+++ app/services/course_service.py
@@ -1,18 +1,18 @@
 from Data.db import courses
 
 def add_course(title, teacher_id):
+    for course in courses:
+        if course["title"].lower() == title.lower():
+            return None
     course_id = len(courses) + 1
-
     course = {
         "id": course_id,
         "title": title,
         "teacher_id": teacher_id,
         "student_ids": []
     }
-
     courses.append(course)
     return course
-
 
 def list_courses():
     return courses
@@ -23,13 +23,13 @@ def assign_student_to_course(course_id, student_id):
         if course["id"] == course_id:
             if student_id not in course["student_ids"]:
                 course["student_ids"].append(student_id)
-            return course
-
-    return None
-
+                return "added"
+            return "already"
+    return "not_found"
 
 def delete_course(course_id):
     for course in courses:
         if course["id"] == course_id:
             courses.remove(course)
-            return
+            return "Le cours a été supprimé"
+    return "Ce cours n'existe pas"
